616dcfa444b218bea2613e29c07473d2fc8ff54a
Matt Berkovits <mberkovits@slack-corp.com>
2025-10-02 13:40:50 +1000

    Update to express

--- app/app.js
+++ app/app.js
@@ -1,14 +1,24 @@
-const http = require('http');
- 
+const express = require('express');
+const app = express();
+const { join } = require('path');
+
 const hostname = '0.0.0.0';
-const port = 8080;
- 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const port = process.env.SIDE_PORT || 8080;
+
+app.use(express.json()); // Parses JSON bodies
+
+app.post('/api/messages', (req, res) => {
+    const message = req.body.message || "No message provided";
+    if (message.toLowerCase() == "ping") {
+        return res.json({ response: "pong" });
+    }
+    return res.json({ response: `Recieved message: ${message}` });
 });
- 
-server.listen(port, hostname, () => {
- console.log(`Server running at http://${hostname}:${port}/`);
+
+app.get('/', (req, res) => {
+    res.send('hello world!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
